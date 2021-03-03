c5d7be8eb8e255028cd7fdaf586d91419f884adf
ange47rm <ange47rm@hotmail.com>
2021-03-03 12:19:41 +0000

    Obtained list of news IDs via fetch request

--- src/containers/NewsContainer.js
+++ src/containers/NewsContainer.js
@@ -1,10 +1,29 @@
-import React from "react"
-import Title from '../components/Title.js'
-import NewsItemList from '../components/NewsItemList.js'
+import React, { useState, useEffect }from "react";
+import Title from '../components/Title.js';
+import NewsItemList from '../components/NewsItemList.js';
 import Form from '../components/Form.js'
 
 
 const NewsContainer = () => {
+
+    const [news, setNews] = useState([]);
+
+    const [newsIds, setNewsIds] = useState([]);
+    
+    const getNewsIds = () => {
+        console.log("You getting some HackerNews!")
+        // fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`)
+        fetch(`https://hacker-news.firebaseio.com/v0/topstories.json`)
+        .then(res => res.json())
+        .then(data => setNewsIds(data))
+
+    }
+
+    useEffect(() => {
+        console.log("UseEffect triggered by getNewsIds")
+        getNewsIds();
+    }, [])
+
     return (
 
         <>
